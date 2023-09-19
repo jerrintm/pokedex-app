a58133d3dee60ac4189604dde1dcd274c8c08a03
jerrintm <140678144+jerrintm@users.noreply.github.com>
2023-09-19 15:39:17 -0400

    Removed code for Task 1 and defined Pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
-alert('Hello world');
-let favoriteFood = 'burger';
-document.write(favoriteFood);
+let pokemonList = []
+pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types['poison', 'fire']  
+},
+    {
+        name: 'Charmander',
+        height: 6,
+        types ['fire', 'ice']
+},
+    {
+        name: 'Fearow',
+        height: 10,
+        types ['bug', 'grass']
+}
+
+
+]
+
+//alert('Hello world');
+//let favoriteFood = 'burger';
+//document.write(favoriteFood);
