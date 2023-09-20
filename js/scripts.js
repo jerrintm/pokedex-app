4a2281779b95fe8b3ed697bff61d1ea457073f84
jerrintm <140678144+jerrintm@users.noreply.github.com>
2023-09-19 22:56:01 -0400

    Added Task 1.3 details

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemonList = []
+let pokemonList = [];
 pokemonList = [
     {
         name: 'Bulbasaur',
@@ -16,8 +16,19 @@ pokemonList = [
         types ['bug', 'grass']
 }
 
-
 ]
+
+
+for (let i = 0; i < pokemonList.length; i++)    /* pokemonList.length will pull the length of the pokemonList array itself. */ {
+    if height[i] > 1
+            {
+        document.write(pokemonList.name[i] + "height:" + pokemonList.height[i] + "Wow, that's big!");
+    }
+    document.write(pokemonList.name[i] + "height:" + pokemonList.height[i]);
+}
+
+
+
 
 //alert('Hello world');
 //let favoriteFood = 'burger';
