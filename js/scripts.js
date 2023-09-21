06df03d9815b7759c32760d717b7459e6bee8607
jerrintm <140678144+jerrintm@users.noreply.github.com>
2023-09-20 23:46:42 -0400

    replaced for loop with foreach()

--- js/scripts.js
+++ js/scripts.js
@@ -19,17 +19,35 @@ pokemonList = [                       /* array definition */
 ];
 
 
-for (let i = 0; i < pokemonList.length; i++) {   /* for loop to pull objects from the array. pokemonList.length will pull the length of the pokemonList array itself. */
-    if (pokemonList[i].height > 9) {            /* checking height object from pokemonList array */
-        document.write(pokemonList[i].name + "," + " " + "height:" + pokemonList[i].height + " - " + "Wow, that's big!" + "<br>");  /* writes output to the screen 'DOM', br adds a new line, details within double quotes are taken as a single string */
+
+/*   .....................................  The below for loop code was replaced with the foreach() which does the same job ..................................       */
+
+//for (let i = 0; i < pokemonList.length; i++) {   /* for loop to pull objects from the array. pokemonList.length will pull the length of the pokemonList array itself. */
+//if (pokemonList[i].height > 9) {            /* checking height object from pokemonList array */
+//    document.write(pokemonList[i].name + "," + " " + "height:" + pokemonList[i].height + " - " + "Wow, that's big!" + "<br>");  /* writes output to the screen 'DOM', br adds a new line, details within double quotes are taken as a single string */
+//}
+//else
+
+//    document.write(pokemonList[i].name + "," + " " + "height:" + pokemonList[i].height);
+//document.write("<br>");            /* this line can be ommitted by adding br to the above document.write line itself */
+//};
+
+
+/*  .......................................  The below foreach() replaces the above forloop code serving the same outcome  ....................................      */
+
+pokemonList.forEach(function (item, index) {         /* the foreach() is used to iterate through each element in the array, it returns the array element and array index. Here since there is are array of objects, 'item' will return each element, the 'index' value is used to reference each object within the array. The function is used within foreach to send pull the array values from the pokemonList array  */
+    if (pokemonList[index].height > 9) {            /* checking height object from pokemonList array */
+        document.write(pokemonList[index].name + "," + " " + "height:" + pokemonList[index].height + " - " + "Wow, that's big!" + "<br>");  /* writes output to the screen 'DOM', br adds a new line, details within double quotes are taken as a single string */
     }
     else
 
-        document.write(pokemonList[i].name + "," + " " + "height:" + pokemonList[i].height);
+        document.write(pokemonList[index].name + "," + " " + "height:" + pokemonList[index].height);
     document.write("<br>");            /* this line can be ommitted by adding br to the above document.write line itself */
-};
 
 
+
+
+})
 
 
 //alert('Hello world');               /* alert displays the text in a pop up */
